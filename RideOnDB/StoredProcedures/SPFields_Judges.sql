5d0837ae0b8d09b85b671bd0adce951cb7928477
orenahuvia <orenn12321@gmail.com>
2026-04-02 17:45:34 +0300

    Update SPFields_Judges.sql

--- RideOnDB/StoredProcedures/SPFields_Judges.sql
+++ RideOnDB/StoredProcedures/SPFields_Judges.sql
@@ -11,8 +11,8 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE SP_GetJudgesByField
-    @FieldId TINYINT
+CREATE PROCEDURE usp_GetAllJudges
+    @FieldId TINYINT = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -23,14 +23,23 @@ BEGIN
         J.LastNameHebrew,
         J.FirstNameEnglish,
         J.LastNameEnglish,
-        J.Country
+        J.Country,
+        -- קיבוץ כל הענפים של השופט למחרוזת אחת מופרדת בפסיקים לטובת התצוגה:
+        (
+            SELECT STRING_AGG(F.FieldName, ', ')
+            FROM JudgeField JF
+            INNER JOIN Field F ON JF.FieldId = F.FieldId
+            WHERE JF.JudgeId = J.JudgeId
+        ) AS QualifiedFields
     FROM Judge J
-    INNER JOIN JudgeField JF ON J.JudgeId = JF.JudgeId
-    WHERE JF.FieldId = @FieldId
-    ORDER BY J.FirstNameHebrew;
+    -- הסינון: נביא את השופט רק אם הוא משויך לענף המבוקש (או אם לא נבחר ענף כלל)
+    WHERE (@FieldId IS NULL OR EXISTS (
+        SELECT 1 FROM JudgeField JF2 
+        WHERE JF2.JudgeId = J.JudgeId AND JF2.FieldId = @FieldId
+    ))
+    ORDER BY J.FirstNameHebrew ASC;
 END
 GO
-
 CREATE PROCEDURE SP_GetJudgesByCompetitionId
     @CompetitionId INT
 AS
