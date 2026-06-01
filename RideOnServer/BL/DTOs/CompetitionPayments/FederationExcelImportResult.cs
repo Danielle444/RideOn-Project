82f494905f1d5220cf04b51049889b560c61c614
Danielle444 <kanetidandi@gmail.com>
2026-06-01 17:21:20 +0300

    Add federation Excel import endpoint

---
+++ RideOnServer/BL/DTOs/CompetitionPayments/FederationExcelImportResult.cs
@@ -0,0 +1,19 @@
+﻿namespace RideOnServer.BL.DTOs.CompetitionPayments
+{
+    public class FederationExcelImportResult
+    {
+        public int TotalRows { get; set; }
+
+        public int ImportedCreditsCount { get; set; }
+
+        public int SkippedDuplicatesCount { get; set; }
+
+        public int SkippedZeroAmountCount { get; set; }
+
+        public int FailedRowsCount { get; set; }
+
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
